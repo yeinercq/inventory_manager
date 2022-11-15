b8a7929713e501daeebe0af7893c6c52f36dbacb
YeinerCQ <ycarvajalq@gmail.com>
2022-11-15 17:47:39 -0500

    add products controller and views

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,59 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @products = current_company.products.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = current_company.products.build
+  end
+
+  def create
+    @product = current_company.products.build(product_params)
+    @product.user = current_user
+    if @product.save
+      redirect_to products_path, notice: "Product was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to products_path, notice: "Product was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path, notice: "Product was successfully destroyed."
+  end
+
+  private
+
+  def set_product
+    @product = current_company.products.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :brand,
+      :description,
+      :unit,
+      :size,
+      :price,
+      :provider_id,
+      :initial_quantity
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :providers, except: [:show]
   resources :customers
+  resources :products
 end
