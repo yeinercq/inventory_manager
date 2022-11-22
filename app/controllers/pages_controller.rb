420e368445a32d186ec127747f7fd601a5364418
YeinerCQ <ycarvajalq@gmail.com>
2022-11-21 21:58:52 -0500

    add home page objects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!
-  
+
   def home
+    @products = Product.sale_priced.ordered
+    @company = Company.find_by(name: "Coopkahawa")
   end
 end
