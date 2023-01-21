03e47c6485767f93760a7266646b160cb4656314
YeinerCQ <ycarvajalq@gmail.com>
2023-01-20 20:14:43 -0500

    modal behavior to wallets configuration

--- app/controllers/general_settings_controller.rb
+++ app/controllers/general_settings_controller.rb
@@ -8,10 +8,13 @@ class GeneralSettingsController < ApplicationController
   def create
     @general_setting = GeneralSetting.new(general_setting_params)
     @general_setting.company = current_company
-    if @general_setting.save
-      redirect_to wallets_path, notice: t('general_settings.created_success')
-    else
-      render :new, status: :unprocessable_entity
+    respond_to do |format|
+      if @general_setting.save
+        format.html { redirect_to wallets_path, notice: t('general_settings.created_success') }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.turbo_stream { render :form_update, status: :unprocessable_entity }
+      end
     end
   end
 
