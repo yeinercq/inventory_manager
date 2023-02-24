5bf0f26a568908222f662067fad1a12f35e67539
YeinerCQ <ycarvajalq@gmail.com>
2023-02-24 13:45:26 -0500

    add location views to coffe_purchases

--- app/controllers/coffee_purchases_controller.rb
+++ app/controllers/coffee_purchases_controller.rb
@@ -1,16 +1,17 @@
 class CoffeePurchasesController < ApplicationController
+  before_action :set_location
   before_action :set_coffee_purchase, only: [:show, :edit, :update, :destroy, :trigger]
 
-  def index
-    # @coffee_purchases = current_company.coffee_purchases.limit(10).ordered
-    @coffee_purchases = current_company.coffee_purchases.where(nil).ordered.first(10)
-    filtering_params(params).each do |key, value|
-      @coffee_purchases = current_company.coffee_purchases.public_send("filter_by_#{key}", value).ordered if value.present?
-    end
-    if params[:start_date].present? and params[:end_date].present?
-      @coffee_purchases =  current_company.coffee_purchases.filter_by_date(Date.parse(params[:start_date]).beginning_of_day, Date.parse(params[:end_date]).end_of_day).ordered
-    end
-  end
+  # def index
+  #   # @coffee_purchases = current_company.coffee_purchases.limit(10).ordered
+  #   @coffee_purchases = current_company.coffee_purchases.where(nil).ordered.first(10)
+  #   filtering_params(params).each do |key, value|
+  #     @coffee_purchases = current_company.coffee_purchases.public_send("filter_by_#{key}", value).ordered if value.present?
+  #   end
+  #   if params[:start_date].present? and params[:end_date].present?
+  #     @coffee_purchases =  current_company.coffee_purchases.filter_by_date(Date.parse(params[:start_date]).beginning_of_day, Date.parse(params[:end_date]).end_of_day).ordered
+  #   end
+  # end
 
   def show
   end
@@ -21,10 +22,11 @@ class CoffeePurchasesController < ApplicationController
 
   def create
     @coffee_purchase = current_user.coffee_purchases.build(coffee_purchase_params)
+    @coffee_purchase.location = @location
     @coffee_purchase.destare_quantity = current_company.general_setting.destare_quantity
     if @coffee_purchase.save
       respond_to do |format|
-        format.html { redirect_to coffee_purchases_path, notice: "Coffee purchase sucessfuly created." }
+        format.html { redirect_to location_path(@location), notice: "Coffee purchase sucessfuly created." }
         format.turbo_stream { flash.now[:notice] = "Coffee purchase sucessfuly created." }
       end
     else
@@ -38,7 +40,7 @@ class CoffeePurchasesController < ApplicationController
   def update
     if @coffee_purchase.update(coffee_purchase_params)
       respond_to do |format|
-        format.html { redirect_to coffee_purchases_path, notice: "Coffee purchase sucessfuly updated." }
+        format.html { redirect_to location_path(@location), notice: "Coffee purchase sucessfuly updated." }
         format.turbo_stream {flash.now[:notice] = "Coffee purchase sucessfuly updated."}
       end
     else
@@ -49,7 +51,7 @@ class CoffeePurchasesController < ApplicationController
   def destroy
     if @coffee_purchase.destroy
       respond_to do |format|
-        format.html { redirect_to coffee_purchases_path, notice: "Coffee purchase sucessfuly destroyed." }
+        format.html { redirect_to location_path(@location), notice: "Coffee purchase sucessfuly destroyed." }
         format.turbo_stream { flash.now[:notice] = "Coffee purchase sucessfuly destroyed." }
       end
     end
@@ -70,6 +72,10 @@ class CoffeePurchasesController < ApplicationController
 
   def filtering_params(params)
     params.slice(:status, :client_id_number, :code, :coffee_type)
+  end
+
+  def set_location
+    @location = current_company.locations.find(params[:location_id])
   end
 
   def set_coffee_purchase
